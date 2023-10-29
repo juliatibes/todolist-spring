960e3c9dfe0b08b131edc67078987c99e97ece25
juliatibes <julia.tibes@gmail.com>
2023-10-29 13:30:59 -0300

    Commit do projeto
    Projeto final

---
+++ src/main/java/br/dev/julia/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,61 @@
+package br.dev.julia.todolist.filter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.dev.julia.todolist.user.IUserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository UserRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+
+        if (servletPath.startsWith("/tasks/")) {
+
+            // Pegar usuário e senha
+            var authorization = request.getHeader("Authorization");
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecoded);
+
+            String[] credencials = authString.split(":");
+            String username = credencials[0];
+            String password = credencials[1];
+
+            System.out.println(username);
+            System.out.println(password);
+
+            // Valida usuário
+            var user = this.UserRepository.findByUsername(username);
+            if(user == null) {
+                response.sendError(401);
+            } else {
+                // Valida senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    // Segue com a requisição
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
